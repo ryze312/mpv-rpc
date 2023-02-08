07218def6575cbb9754ae8589c3cc7846d291bc2
Ryze <ryze312@tutanota.com>
2023-02-08 22:48:57 +0300

    Added installer script and assets for it

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mpv_home="${MPV_HOME:-${XDG_CONFIG_HOME:-${HOME}/.config}/mpv}"
+scripts_dir="$mpv_home/scripts"
+
+if [ -d $scripts_dir ]; then
+    mkdir -p "$scripts_dir"
+fi
+
+echo -n "Copying script..."
+cp ./bin/libmpv_rpc.so "$scripts_dir"
+echo "Done!"
+
+echo -n "Copying default config..."
+cp ./config/rpc.json "$mpv_home"
+echo "Done!"
+
+echo -n "Adding keybinding entry to input.conf..."
+echo 'D script-binding "libmpv_rpc/toggle-rpc"' > "$mpv_home/input.conf"
+echo "Done!"
