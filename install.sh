e95168ccf4741f2d435946ff90fdf4a56bea0a94
Ryze <ryze312@tutanota.com>
2023-02-12 15:00:06 +0300

    Add keybinding only if doesn't exist

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+key='D'
+script_binding='script-binding "libmpv_rpc/toggle-rpc"'
 mpv_home="${MPV_HOME:-${XDG_CONFIG_HOME:-${HOME}/.config}/mpv}"
 scripts_dir="$mpv_home/scripts"
 
@@ -15,6 +17,8 @@ echo -n "Copying default config..."
 cp ./config/rpc.json "$mpv_home"
 echo "Done!"
 
-echo -n "Adding keybinding entry to input.conf..."
-echo 'D script-binding "libmpv_rpc/toggle-rpc"' >> "$mpv_home/input.conf"
-echo "Done!"
+if ! grep -q "$script_binding" "$mpv_home/input.conf"; then
+    echo -n "Adding keybinding entry to input.conf..."
+    echo "$key $script_binding" >> "$mpv_home/input.conf"
+    echo "Done!"
+fi
